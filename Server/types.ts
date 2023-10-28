a2ce55580940a2f9035fdd1fd7db7353690ba5c7
Wenhao <wenhaoyang2015@gmail.com>
2023-10-28 19:49:58 -0400

    Added SocketInfo and Result types
    Also added socketTable in users.ts to map sockets to socket info
    
    Result type makes it easier to differentiate successful and unsuccessful results
    
    Co-authored-by: Garrett Hinkley <RedDog739@users.noreply.github.com>

--- Server/types.ts
+++ Server/types.ts
@@ -1,16 +1,50 @@
 type Faction = "T" | "M" | "S" | "A";
+
 enum Team {
     Red = 0,
     Blue = 1
 }
+
+enum SocketState {
+    Menu = 0,
+    Lobby = 1,
+    Game = 2
+}
+
+type SocketInfo = {
+    state: SocketState,
+    clientId: string,
+    info: { user: User, lobby: Lobby } | { player: Player, game: Game }
+}
+
 type User = {
-    socketId: string,
+    id: string, // Server side, this is socketId; client side, this is client id
+    name: string
     faction: Faction,
     team: Team,
-    name: string // add size limit, maybe truncate at size limit automatically (and also prevent limit on client side)
 };
+
 type Lobby = {
     users: User[],
     id: string,
     started: boolean
 };
+
+type Result<T, U> = {
+    isSuccessful: boolean,
+    value: T | U
+}
+
+function success<T, U>(value: T): Result<T, U> {
+    return {
+        isSuccessful: true,
+        value: value
+    }
+}
+
+function failure<T, U>(value: U): Result<T, U> {
+    return {
+        isSuccessful: false,
+        value: value
+    }
+}
---
+++ Server/users.ts
@@ -0,0 +1,1 @@
+const socketTable:{[id: string]: SocketInfo} = {}
