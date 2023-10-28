3cba26cb5be4bd62eff1c5efae6afd3aae4baddd
Wenhao <wenhaoyang2015@gmail.com>
2023-10-28 19:43:20 -0400

    Added functions.js in Client folder.
    Created isValidName function in functions.js
    
    Modified tsconfig.json to allowJS
    
    Co-authored-by: Garrett Hinkley <RedDog739@users.noreply.github.com>

---
+++ Client/functions.js
@@ -0,0 +1,5 @@
+// Validates an input name
+// Currently, only specifications is that the type is a string and name is not empty but also not too long.
+export function isValidName(name) {
+    return typeof(name) === "string" && name.length > 0 && name.length <= MAX_NAME_SIZE;
+}
