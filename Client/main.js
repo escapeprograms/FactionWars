4b3cdd3fa41b69cd39f8525d6835cbb16c6a374f
Garrett Hinkley <reddog7391@gmail.com>
2023-11-04 17:38:15 -0400

    Add Rect class

--- Client/main.js
+++ Client/main.js
@@ -1,3 +1,20 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+// parameters x and y represent top left corner unless specified otherwise
+
+class Rect {
+    constructor(x, y, width, height, color = "white") {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
