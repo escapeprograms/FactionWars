fae48aec65559d35c6ee56490632f03ae22c478b
Garrett Hinkley <reddog7391@gmail.com>
2023-11-04 17:38:46 -0400

    Add Text class

--- Client/main.js
+++ Client/main.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+// see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+context.font = "20px sans-serif";
+context.textAlign = "center";
+context.textBaseline = "middle";
+
 // parameters x and y represent top left corner unless specified otherwise
 
 class Rect {
@@ -15,6 +20,20 @@ class Rect {
     draw() {
         context.fillStyle = this.color;
         context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Text {
+    // x and y represent center
+    constructor(x, y, text) {
+        this.x = x;
+        this.y = y;
+        this.text = text;
+    }
+
+    draw() {
+        context.fillStyle = "black";
+        context.fillText(this.text, this.x, this.y);
     }
 }
 
