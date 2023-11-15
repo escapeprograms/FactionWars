e629cbb5820202bcb8781d52851e95a3bfc4ecf9
Wenhao <wenhaoyang2015@gmail.com>
2023-11-14 19:16:37 -0500

    Fix import problem with constants.js

--- Client/functions.js
+++ Client/functions.js
@@ -1,5 +1,7 @@
+import {MAX_NAME_SIZE} from "./constants.js"
+
 // Validates an input name
 // Currently, only specifications is that the type is a string and name is not empty but also not too long.
 export function isValidName(name) {
-    return typeof(name) === "string" && name.length > 0 && name.length <= MAX_NAME_SIZE;
+    return typeof (name) === "string" && name.length > 0 && name.length <= MAX_NAME_SIZE;
 }
