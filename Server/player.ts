19c6fcd110d35939f5f4d7d08a5b9f7011dcc5ad
Wenhao <wenhaoyang2015@gmail.com>
2024-03-20 15:53:18 -0400

    Bugfix
    Update Events for player.ts as well

--- Server/player.ts
+++ Server/player.ts
@@ -1,5 +1,5 @@
 import { Coordinate, PlayerId, Events, SocketEvent, Card, Deck, GameState,emptyPArr, play } from "./types.js";
-import { arrEqual, concatEvents, doubleIt, isIntInRange } from "./utility.js";
+import { arrEqual, doubleIt, isIntInRange } from "./utility.js";
 import { MAX_HAND_SIZE } from "../Client/constants.js";
 
 export class PlayerInfo {
@@ -16,35 +16,37 @@ export class PlayerInfo {
         this.self = self;
     }
     draw(): Events {
-        const ret = emptyPArr<SocketEvent>();
+        const ret = new Events();
         const card = this.deck.draw();
         if (!card) {console.log("No card drawn"); return ret;} // Potentially change later, but for now, the draw just fails
-        doubleIt((i, j) => 
+        /*doubleIt((i, j) => 
             ret[i][j].push({event: "card-drawn", params: (i === this.self[0] && j === this.self[1] ? [[...this.self], card] : [[...this.self]])}), 
-        0, 0, 2, 2);
+        0, 0, 2, 2);*/
+        ret.addFilteredEvent("card-drawn", [[...this.self], card], [[...this.self]], (i, j) => i === this.self[0] && j === this.self[1]);
         this.cards.push(card); // Add card to hand
         if (this.cards.length > MAX_HAND_SIZE) {
-            concatEvents(ret, this.discard(this.cards.length - 1)); // Immediately discard; hand is full
+            ret.concat(this.discard(this.cards.length - 1)); // Immediately discard; hand is full
             console.log("Hand is full, drawn card immediately discarded");
         }
         return ret;
     }
     discard(index: number): Events {
-        const ret = emptyPArr<SocketEvent>();
+        const ret = new Events();
         if (!isIntInRange(index, 0, this.cards.length - 1)) return ret; // Invalid discard
         this.deck.add(this.cards.splice(index, 1)[0]);
-        doubleIt((i, j) => ret[i][j].push({event: "card-discarded", params: [...this.self, index]}), 0, 0, 2, 2);
+        //doubleIt((i, j) => ret[i][j].push({event: "card-discarded", params: [...this.self, index]}), 0, 0, 2, 2);
+        ret.addEvent("card-discarded", [...this.self, index]);
         return ret;
     }
     // Deactivates buildings until energy is nonnegative
     // Pass in game
     upkeep(game: GameState): Events {
-        const ret = emptyPArr<SocketEvent>();
+        const ret = new Events();
         const buildings = game.buildings;
         let i = buildings.length;
         while (this.energy < 0) {
             if (arrEqual(this.self, buildings[--i].owner) && buildings[i].stats.upkeep > 0) {
-                concatEvents(ret, buildings[i].deactivate(game));
+                ret.concat(buildings[i].deactivate(game));
             }
         }
         return ret;
@@ -54,10 +56,10 @@ export class PlayerInfo {
     play(game: GameState, index: number, targets: {[key: string]: any}): Events {
         //const self = game.get(this.self, "player") as Player; //
         const self = game.getPlayer(this.self);
-        const ret = emptyPArr<SocketEvent>();
+        const ret = new Events();
         // can only play on your turn and can only play cards that exist
         if (game.turn === self.team && isIntInRange(index, 0, this.cards.length - 1)) {
-            concatEvents(ret, play(game, this.self, index, targets));
+            ret.concat(play(game, this.self, index, targets));
         }
         return ret;
     }
